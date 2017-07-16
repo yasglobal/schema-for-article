1d05bf61ceb0059f49a9b75f30be91e6f6ceba52
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2017-07-16 19:26:10 +0500

    This is a fully functional version based on the idea of minimum viable product.

---
+++ readme.txt
@@ -0,0 +1,49 @@
+﻿=== SCHEMA for Article ===
+Contributors: sasiddiqui
+Tags: json-ld, markup, schema, rich snippets, structured data, SEO, schema.org, schema markup, JSON, google validated
+Requires at least: 3.5
+Tested up to: 4.4
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+SCHEMA for Article is simply the easiest solution to add valid schema.org as a JSON script in the head of blog posts or articles.
+
+== Description ==
+Search engines such as Google are using structured data markup in many ways—for example, to create rich snippets in search results. Search results with rich snippets will improve your click through rates and increase the number of visitors on your website.
+
+This plugin helps:
+
+* Helps your site to earn rich snippets in Google’s SERP.
+* Does not depend on other plugins or external code.
+* Is simple to install: plug-and-play, no need to configure anything.
+
+
+== Installation ==
+
+The easiest way to install the plugin is to use the WP built-in menu for finding and installing new plugins directly from the WordPress repositories.
+
+If you need to install this using FTP or SFTP, you should follow these steps:
+
+1. Upload `json-ld-article.php` to the `/wp-content/plugins/` directory using your favorite FTP client.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. That’s it.
+
+
+**Compatibility.** This version requires php 5.4 for some options of json_encode. If you encounter any problems with the plugin you should check your web hotel’s php version.
+
+== Frequently Asked Questions ==
+
+**Why should I install this plugin?**
+*** Installing this plugin is the easiest way to add structured data to your blog. The plugin automatically creates the JSON-LD according to Google’s specification. 
+
+**Does this plugin improve my SEO rankings?** 
+*** We cannot promise it - but installing this plugin is in any case a step in right direction.***
+
+**Does image necessarily to be provided to activate the plugin**
+***Yes, Incase the fearuted image is not provided by the author so, the defualt provided image would be use to validate the google structure data***
+
+== Changelog ==
+
+= 0.1 =
+*  This is a fully functional version based on the idea of minimum viable product.
---
+++ schema-for-article.php
@@ -0,0 +1,220 @@
+<?php 
+
+/*
+Plugin Name: Schema for Article
+Plugin URI: https://wordpress.org/plugins/schema-for-article/
+Description: Schema for Article is simply the easiest solution to add valid schema.org as a JSON script in the head of blog posts or posts. The provided LD-JSON is validated from the google structured data.
+Version:     0.1
+Author:      Sami Ahmed Siddiqui
+Author URI: http://www.yasglobal.com/
+Text Domain: schema-for-article
+*/
+
+function schema_article_settings_link($links) { 
+  $settings_link = '<a href="admin.php?page=schema-article-settings">Settings</a>'; 
+  array_unshift($links, $settings_link); 
+  return $links; 
+}
+
+function schema_article_menu() {
+	add_menu_page('SCHEMA.ORG ARTICLE SETTINGS', 'SCHEMA.ORG ARTICLE Settings', 'administrator', 'schema-article-settings', 'schema_article_settings_page');
+   add_action( 'admin_init', 'register_schema_article_settings' );
+}
+
+function register_schema_article_settings() {
+   register_setting( 'schema-article-settings-group', 'schema_article_org_name' );
+   register_setting( 'schema-article-settings-group', 'schema_article_logo' );
+   register_setting( 'schema-article-settings-group', 'schema_article_image' );
+   register_setting( 'schema-article-settings-group', 'schema_article_activate' );
+}
+
+function schema_article_styles() {
+   wp_register_style( 'style', plugins_url('/style.css', __FILE__) );
+   wp_enqueue_style( 'style' );
+}
+
+function schema_article_settings_page() {
+	if ( !current_user_can( 'administrator' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+   echo '<div class="wrap">';
+   echo '<h2>SCHEMA.ORG ARTICLE SETTINGS</h2>';
+   echo '<div>Define the Permalinks for each post type. You can define different structures for each post type. </div>';
+   echo '<form method="post" action="options.php">';
+   settings_fields( 'schema-article-settings-group' );
+   do_settings_sections( 'schema-article-settings-group' );
+   $schema_name = esc_attr( get_option('schema_article_org_name') );
+   $schema_activated = esc_attr( get_option('schema_article_activate') );
+   $schema_activated_value = "on";
+   $schema_activated_checked = "";
+   if(empty($schema_name)){
+      $schema_name = get_bloginfo("name");
+   }
+   if($schema_activated == 'on'){
+      $schema_activated_value = "off";
+      $schema_activated_checked = "checked";
+   }
+   ?>
+   <table class="schema-admin-table">
+   	<caption>Basic Setting</caption>
+	   <tr>
+         <th>headline :</th>
+         <td><small>Default : post_title</small></td>
+      </tr>
+	   <tr>
+         <th>datePublished :</th>
+         <td><small>Default : get_the_time( DATE_ISO8601, ID )</small></td></tr>
+	   <tr>
+         <th>dateModified :</th>
+         <td><small>Default : get_the_modified_time( DATE_ISO8601, false, ID )</small></td></tr>
+	   <tr>
+         <th>description :</th>
+         <td><small>Default : post_excerpt</small></td>
+      </tr>
+	</table>
+	
+   <table class="schema-admin-table">
+	   <caption>mainEntityOfPage ( recommended )</caption>
+	   <tr>
+         <th>@type :</th><td><small>"WebPage"</small></td>
+      </tr>
+	   <tr>
+         <th>@id :</th>
+         <td><small>Default : get_permalink( ID )</small></td>
+      </tr>
+	</table>
+	
+   <table class="schema-admin-table">
+	   <caption>image ( required )</caption>
+	   <tr>
+         <th>@type :</th>
+         <td><small>"ImageObject"</small></td></tr>
+		<tr>
+         <th>url :</th>
+         <td>
+            <label for="upload_image">
+               <input type="text" name="schema_article_image" id="logo" class="regular-text" required value="<?php echo esc_attr( get_option('schema_article_image') ); ?>" />
+               <small>Default: thumbnail(Featured Image)<br />When Featured image is not available then this image would be shown in the markup</small>
+            </label>
+         </td>
+      </tr>
+		<tr>
+         <th>height :</th>
+         <td><small>Auto : The height of the image, in pixels.</small></td>
+      </tr>
+		<tr>
+         <th>width :</th>
+         <td><small>Auto : The width of the image, in pixels. Images should be at least 696 pixels wide.</small></td>
+      </tr>
+   </table>
+	
+   <table class="schema-admin-table">
+		<caption>publisher( required )</caption>
+		<tr>
+         <th>@type :</th>
+         <td><small>"Organization"</small></td>
+      </tr>
+		<tr>
+         <th><label for="name">Organization Name :</label></th>
+         <td><input type="text" name="schema_article_org_name" id="name" class="regular-text" required value="<?php echo $schema_name; ?>" /><small>Default : bloginfo("name")</small></td>
+      </tr>
+	</table>
+	
+   <table class="schema-admin-table">
+	   <caption>logo ( required )</caption>
+		<tr><th>@type :</th><td><small>"ImageObject"</small></td></tr>
+		<tr>
+         <th><label for="logo">url :</label></th>
+         <td><input type="text" name="schema_article_logo" id="logo" class="regular-text" required value="<?php echo esc_attr( get_option('schema_article_logo') ); ?>" /></td>
+      </tr>
+		<tr>
+         <th>height :</th>
+         <td><small>Auto : height >= 60px.</small></td>
+      </tr>
+		<tr><th>width :</th><td><small>Auto : width <= 600px.</small></td></tr>
+	</table>
+
+		<table class="schema-admin-table">
+         <tbody>
+            <tr>
+               <td><input type="checkbox" name="schema_article_activate" value="<?php echo $schema_activated_value; ?>" <?php echo $schema_activated_checked; ?> /><strong>Activate SCHEMA.ORG Article</strong></td>
+            </tr>
+         </tbody>
+      </table>
+
+		<p>Setting Knowledge : <a href="https://developers.google.com/structured-data/rich-snippets/articles" target="_blank">https://developers.google.com/structured-data/rich-snippets/articles</a></p> 
+   <?php
+   submit_button(); 
+   echo '</form>';
+   echo '</div>';
+}
+
+function schema_article_add_json_markup(){
+	global $post;
+   if ( esc_attr( get_option('schema_article_activate') ) == "on" ) {
+      $schema_name = esc_attr( get_option('schema_article_org_name') );
+      $schema_logo = esc_attr( get_option('schema_article_logo') );
+      $schema_image = esc_attr( get_option('schema_article_image') );
+		$images = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+		if(!isset($images) && empty($images)){
+         $images[0] = $schema_image;
+      }
+		$excerpt = schema_article_escape_text_tags($post->post_excerpt);
+		$content = $excerpt === "" ? mb_substr( schema_article_escape_text_tags( $post->post_content ), 0, 110 ) : $excerpt;
+		$args = array(
+				"@context" => "http://schema.org",
+				"@type"    => "Article",
+				"mainEntityOfPage" => array(
+					"@type" => "WebPage",
+					"@id"   => get_permalink( $post->ID )
+				),
+				"headline" => schema_article_escape_text_tags( $post->post_title ),
+				"image"    => array(
+					"@type"  => "ImageObject",
+					"url"    => $images[0],
+					"width"  => "auto",
+					"height" => "auto"
+				),
+				"datePublished" => get_the_time( DATE_ISO8601, $post->ID ),
+				"dateModified"  => get_post_modified_time(  DATE_ISO8601, __return_false(), $post->ID ),
+				"author" => array(
+					"@type" => "Person",
+					"name"  => schema_article_escape_text_tags( get_the_author_meta( 'display_name', $post->post_author ) )
+				),
+				"publisher" => array(
+					"@type" => "Organization",
+					"name"  => $schema_name,
+					"logo"  => array(
+					   "@type"  => "ImageObject",
+					   "url"    => $schema_logo,
+					   "width"  => "auto",
+					   "height" => "auto"
+				   )
+			   ),
+			   "description" => $content
+		      );
+		schema_article_set_schema_json( $args );
+	}
+}
+
+function schema_article_escape_text_tags ( $text ) {
+	return (string) str_replace( array( "\r", "\n" ), '', strip_tags( $text ) );
+}
+
+function schema_article_set_schema_json ( array $args ) {
+	echo '<script type="application/ld+json">' , PHP_EOL;
+	echo json_encode( $args, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT ) , PHP_EOL;
+	echo '</script>' , PHP_EOL;
+}
+
+if(function_exists("add_action") && function_exists("add_filter")) { 
+   $plugin = plugin_basename(__FILE__); 
+   add_filter("plugin_action_links_$plugin", 'schema_article_settings_link' );
+   
+   add_action( 'admin_menu', 'schema_article_menu' );
+   add_action ('wp_head','schema_article_add_json_markup');  
+}
+
+if (isset($_GET['page']) && $_GET['page'] == 'schema-article-settings') {
+   add_action('admin_print_styles', 'schema_article_styles');
+}
